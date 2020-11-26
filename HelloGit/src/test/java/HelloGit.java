03bae3213cee76ef7371de0b6ae2a3ccc14f4cbd
shdd <1401033786@qq.com>
2020-11-26 09:53:37 +0800

    版本二

---
+++ HelloGit/src/test/java/HelloGit.java
@@ -0,0 +1,12 @@
+public class HelloGit {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git");
+        System.out.println("版本一");
+        System.out.println("版本二");
+
+
+    }
+
+}
